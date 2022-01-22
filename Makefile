e5e6ae02b214ec3c8e1af3f625fc50a98ac18212
TheDarkBomber <thedarkbomber80@gmail.com>
2022-01-22 18:36:06 +0000

    Bootloader loads disk.

--- Makefile
+++ Makefile
@@ -5,17 +5,24 @@ BUILD=build
 
 BOOTFILE=$(BUILD)/boot.x86
 IMAGEFILE=$(BUILD)/apiofirm.img
+KERNEL=$(BUILD)/system.k
 
-image: boot
-	cp -v $(BOOTFILE) $(IMAGEFILE)
-	truncate -s 1440k $(IMAGEFILE)
+image: boot kernel
+	dd if=/dev/zero of=$(IMAGEFILE) bs=512 count=2880
+	mkfs.fat -F 12 -n "IMAGE" $(IMAGEFILE)
+	dd if=$(BOOTFILE) of=$(IMAGEFILE) conv=notrunc
+	mcopy -i $(IMAGEFILE) $(KERNEL) "::system.k"
 
 boot: $(SOURCES)/boot.asm
 	$(ASSEMBLER) $(SOURCES)/boot.asm -f bin -o $(BOOTFILE)
 
+kernel: $(SOURCES)/kernel.asm
+	$(ASSEMBLER) $(SOURCES)/kernel.asm -f bin -o $(KERNEL)
+
 clean:
 	rm -v $(BOOTFILE)
 	rm -v $(IMAGEFILE)
+	rm -v $(KERNEL)
 
 deepclean:
 	rm -rv $(BUILD)/*
--- src/boot.asm
+++ src/boot.asm
@@ -8,6 +8,35 @@ bits 16													; explicitly produce 16-bit binary files
 %define DOSN 0x0D, 0x0A 				; BIOS TTY mode uses DOS newlines
 %define STRD DOSN, 0 						; Ends string with a DOS newline.
 
+;;;
+;;; FAT12 HEADER
+;;;
+
+jmp short entry
+nop
+
+disk_oem:	db 'CRXBOOT1' 				; OEM, 8 bytes.
+disk_bytes_psector:	dw 512 			; Bytes per sector.
+disk_sectors_pcluster:	db 1 		; Bytes per cluster.
+disk_rsvp_sectors:	dw 1 				; Reserved sectors.
+disk_file_allocation_table_count:	db 2
+disk_directory_entries_count:	dw 0xE0
+disk_total_sectors:	dw 2880 		; 2280 = 1440 * 2
+disk_media_descriptor_type:	db 0xF0 ; F0 = 3.5 inch
+disk_sectors_pfile_allocation_table:	dw 9 ; 9 sectors per file allocation table.
+disk_sectors_ptrack:	dw 18
+disk_heads:	dw 2
+disk_hidden_sectors:	dd 0
+disk_large_sector_count:	dd 0
+
+ebr_drive_number:	db 0 					; 0x00 = floppy disk, 0x80 = hard disk
+ebr_rsvp:	db 0 									; Reserved.
+ebr_signature:	db 0x29
+ebr_volume_id:	db 0x12, 0x34, 0x56, 0x78 ; Serial number.
+ebr_volume_label:	db 'APIOFIRMCRX'				; Label, 11 bytes.
+ebr_system_id:	db 'FAT12   '							; System ID, 11 bytes.
+
+entry:	
 	jmp main 											; Jump to main, unconditionally.
 
 ;;; STRPUT
@@ -46,14 +75,134 @@ main:
 	mov ss, ax 										; Stack setup.
 	mov sp, 0x7C00 								; Origin is 0x7C00, so start stack pointing from there.
 
+	mov [ebr_drive_number], dl 		; DL = drive number, as set by BIOS.
+
+	mov ax, 1 										; LBA = 1
+	mov cl, 1 										; Read 1 sector.
+	mov bx, 0x7E00 								; Data exists after boot code.
+	call diskread 								; Read the disk.
+
 	mov si, message 							; Setup for strput.
 	call strput
 
+	jmp exitboot
+
+;;; DISK ERROR
+;;; When the disk has an error.
+
+disk_error:
+	mov si, disk_read_error_message ; Setup for strput
+	call strput
+	jmp bootpanic
+
+;;; BOOT PANIC
+;;; Waits until user input, then reboots.
+bootpanic:
+	mov si, bootpanic_message 		; Setup for strput
+	call strput
+	mov ah, 0
+	int 0x16 											; Wait for keypress.
+	jmp 0xFFFF:0 									; Jumps to beginning of BIOS, causes a reset.
+
+;;; EXIT BOOT
+;;; Halts the CPU, and makes sure of it.
+exitboot:
+	cli									; Disables interrupts.
 	hlt 													; No more execution is to be done.
 	jmp $ 												; In case CPU restored from halt, become stagnant forever.
 
-;;; variable
+
+;;; CONVERT CHS
+;;; Converts an LBA to CHS.
+;;; Parameter: ax
+;;; LBA address
+;;; Returns: cx, dh
+;;; bits 0-5: sector number
+;;; bits 6-15: cylinder
+;;; head
+convchs:
+	push ax 											; Saves ax, dx.
+	push dx
+
+	xor dx, dx 										; Clears dx.
+	div word [disk_sectors_ptrack] ; ax = LBA / Sectors per track
+
+	inc dx 												; dx = sector = (LBA mod Sectors per track) + 1
+	mov cx, dx 										; cx = sector
+
+	xor dx, dx 										; Clears dx.
+	div word [disk_heads] 				; ax = cylinder =(LBA / Sectors per track) / Heads
+
+	mov dh, dl 										; dh = head
+	mov ch, al 										; ch = cylinder
+	shl ah, 6
+	or cl, ah 										; places upper 2 bits of cylinder in cl
+
+	pop ax 												; Antisave ax, dl
+	mov dl, al
+	pop ax
+	ret
+
+;;; DISK READ
+;;; Parameters: ax, cl, dl, es:bx
+;;; LBA address
+;;; Number of sectors to read
+;;; Drive number
+;;; Memory address where to store data that is read
+diskread:
+	push ax 											; Save registers
+	push bx
+	push cx
+	push dx
+	push di
+
+	push cx 											; Temporary save cl
+	call convchs 									; Convert CHS
+	pop ax 												; al = number of sectors to read
+
+	mov ah, 0x02
+	mov di, 3 										; Retry attempts
+	.retry:
+	pusha 												; Save all registers.
+	stc 													; Set carry flag.
+	int 0x13 											; If carry flag = cleared, ergo success.
+	jnc .endretry 								; Jump if this is the case.
+
+	popa 													; Read failed, antisave all registers.
+	call diskreset 								; Reset disk.
+
+	dec di
+	test di, di
+	jnz .retry
+
+	.failure:
+	jmp disk_error 								; Disk error.
+
+	.endretry:
+	popa 													; Antisave registers.
+	pop di
+	pop dx
+	pop cx
+	pop bx
+	pop ax
+	ret
+
+;;; DISK RESET
+;;; Parameter: dl
+;;; Drive number
+diskreset:
+	pusha
+	mov ah, 0
+	stc
+	int 0x13
+	jc disk_error
+	popa
+	ret
+
+;;; variables
 message:	db 'Bzzzzzzzzzt.', STRD
+disk_read_error_message:	db 'ERR: Could not read disk. Stop.', STRD
+bootpanic_message:	db 'A fatal error was encountered during boot.', DOSN, 'Press any key to reboot...', 0
 
 ;;; Boot sector magic.
 	times 510-($-$$) db 0 				; Pad binary with null until 510th byte.
